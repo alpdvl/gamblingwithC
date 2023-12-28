564db0c1e3edb1bfd95ea070bff7bfd29032b80b
alpdvl <149157860+alpdvl@users.noreply.github.com>
2023-12-28 22:31:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void displayMenu();
 int playroulet(int amount);
 int roller();
 
-// ill fix some text formatting issues and polish others(done?)
+// ill fix some text formatting issues and polish others(done??)
 // ill add true last rolls (later)
 // might add true spinning wheel with so-called animation (done)
 // might add account/login system
@@ -19,7 +19,7 @@ int roller();
 
 int main() {
 
-    //printf("Hello, World!\n");
+    printf("Hello, World!\n");
     srand(time(NULL)); // seeding rand func with time
     displayMenu();
 
@@ -39,8 +39,8 @@ void displayMenu()
     while (choice !=0) {
         system("cls");
 
-        printf("     >>>>> Roul.exe <<<<<\nType what you want to do\n-----------------------------\n");
-        printf("1) Play Roulette\n2) Access Your RoulWallet\n0) Quit\n");
+        printf("     >>>>> Roul.exe <<<<<\n\n|Type what you want to do\n|-----------------------------\n");
+        printf("|1) Play Roulette\n|2) Access Your RoulWallet\n|0) Quit\n>");
         scanf("%d", &choice);
         if (choice == 1) {
             money = playroulet(money);
@@ -49,25 +49,25 @@ void displayMenu()
         {
             while(choice !=9) {
                 system("cls");//windows specific function used in here!
-                printf(">>>>> Roul.exe <<<<<\n1)Deposit\n2)Withdraw\n3)Past Transactions\n9)Back\n");
-                printf("You've: $%d in your RoulWallet\n", money);
+                printf(">>>>> Roul.exe <<<<<\n|1)Deposit\n|2)Withdraw\n|3)Past Transactions\n|9)Back\n-----------------\n");
+                printf("|You've: $%d in your RoulWallet\n", money);
 
 
 
                 scanf("%d", &choice);
                 system("cls");
-                printf("You've: $%d in your RoulWallet\n", money);
+                printf("|You've: $%d in your RoulWallet\n", money);
                 if (choice == 1) {
-                    printf("type the amount which you want to deposit:\n");
+                    printf("|type the amount which you want to deposit:\n$");
                     scanf("%d",&transaction);
 
                     if (transaction >0)
                     {
-                        printf("transaction was succesful!\n");
+                        printf("|transaction was succesful!\n");
                         money = money + transaction;
                         pastactions[transcount+1]= transaction;
                         transcount += 1;
-                        printf("press enter to continue...");
+                        printf("|press enter to continue...");
                         fflush(stdin);
                         fgets(enter,2,stdin);
                         if(enter[0]=='\n')
@@ -75,8 +75,8 @@ void displayMenu()
                     }
                     else
                     {
-                        printf("there was a mistake pls try again\n");
-                        printf("press enter to continue...");
+                        printf("|there was a mistake pls try again\n");
+                        printf("|press enter to continue...");
                         fflush(stdin);
                         fgets(enter,2,stdin);
                         if(enter[0]=='\n')
@@ -84,16 +84,16 @@ void displayMenu()
                     }
 
                 } else if (choice == 2) {
-                    printf("type the amount which you want to withdraw:\n");
+                    printf("|type the amount which you want to withdraw:\n$");
                     scanf("%d",&transaction);
 
                     if (transaction <= money)
                     {
-                        printf("transaction was succesful!\n");
+                        printf("|transaction was succesful!\n");
                         money = money - transaction;
                         pastactions[transcount+1]= -transaction;
                         transcount += 1;
-                        printf("press enter to continue...");
+                        printf("|press enter to continue...");
                         fflush(stdin);
                         fgets(enter,2,stdin);
                         if(enter[0]=='\n')
@@ -101,8 +101,8 @@ void displayMenu()
                     }
                     else
                     {
-                        printf("there was a mistake pls try again\n");
-                        printf("press enter to continue...");
+                        printf("|there was a mistake pls try again\n");
+                        printf("|press enter to continue...");
                         fflush(stdin);
                         fgets(enter,2,stdin);
                         if(enter[0]=='\n')
@@ -111,7 +111,7 @@ void displayMenu()
 
                 } else if (choice == 3) {
                     system("cls");
-                    printf("past transactions..\n");
+                    printf("|past transactions..\n");
                     int i=0;
 
 
@@ -123,7 +123,7 @@ void displayMenu()
 
 
                     }
-                    printf("press enter to continue...");
+                    printf("|press enter to continue...");
                     fflush(stdin);
                     fgets(enter,2,stdin);
                     if(enter[0]=='\n')
@@ -131,7 +131,7 @@ void displayMenu()
                 } else if (choice == 9) {
                     // gate for back
                 } else {
-                    printf("enter something proper pls\n");
+                    printf("|enter something proper pls\n");
                 }
             }
 
@@ -150,26 +150,32 @@ int playroulet(int amount)
     int betamount=0;
     int money = amount;
     system("cls");
-    printf("last rolls were: 5(b) 12(r) 17(b) 28(r) 31(b)\n"); // ill fix this later
-    printf("you're playing roulette please enter your bet color\n"
-           "\n1)Black\n2)Red\n3)Green\n4)Cancel the action...");
+    printf("|last rolls were: 5(b) 12(r) 17(b) 28(r) 31(b)\n----------------------\n"); // ill fix this later
+    printf("|you're playing roulette please enter your bet color\n"
+           "\n|1)Black\n|2)Red\n|3)Green\n|4)Cancel the action...");
     scanf("%d",&color);
+    system("cls");
     if(color==1)
-    { printf("your choice is Black\n");
-        printf("please enter your bet amount\n$");
+    { printf("|your choice was Black\n");
+        printf("|please enter your bet amount\n$");
         scanf("%d",&betamount);}
     else if(color == 2)
-    { printf("your choice is red\n");
-        printf("please enter your bet amount\n$");
+    { printf("|your choice was red\n");
+        printf("|please enter your bet amount\n$");
         scanf("%d",&betamount);}
     else if(color == 3)
-    { printf("your choice is green\n");
-        printf("please enter your bet amount\n$");
+    { printf("|your choice was green\n");
+        printf("|please enter your bet amount\n$");
         scanf("%d",&betamount);}
     else if(color ==4)
-    { printf("you're heading back to choosing color menu\n");
+    { printf("|you're heading back to choosing color menu\n");
         }
-    if (color != 4)
+    if (money < betamount)
+    {
+        printf("|hold on there are you trying to rob us???\n|You're heading back to color menu...\n");
+        Sleep(2000);
+    }
+    else if (color != 4)
     {
         money = money - betamount;
         roll = roller();
@@ -178,23 +184,23 @@ int playroulet(int amount)
             result = 3;
             if(color==3)
             {
-                printf("congrats you hit the green!\n");
+                printf("|congrats you hit the green!\n");
                 money= money + (betamount *14);
 
             }
-            else  { printf("you missed the green fortune...");}
+            else  { printf("|you missed the green fortune...");}
         }
         else {
             if (color == 1) {
                 if (roll % 2 == 1)
                 {
-                    printf("congrats you hit black!\n");
+                    printf("|congrats you hit black!\n");
                     money= money + (betamount * 2);
                     result = 1;
                 }
                 else
                 {
-                    printf("you lose!\n");
+                    printf("|you lost!\n");
                     result = 2;
                 }
             }
@@ -202,26 +208,26 @@ int playroulet(int amount)
             else{
                 if (roll % 2 ==0)
                 {
-                    printf("congrats you hit red!\n");
+                    printf("|congrats you hit red!\n");
                     money= money +(betamount *2);
                     result = 2;
                 }
                 else
                 {
-                    printf("you lose!\n");
+                    printf("|you lost!\n");
                     result = 1;
                 }
             }
         }
         if (result ==1)
-        {printf("ROLL WAS: %s\n",black);}
+        {printf("|ROLL WAS: %s\n",black);}
         else if(result==2)
-        {printf("ROLL WAS: %s\n",red);}
+        {printf("|ROLL WAS: %s\n",red);}
         else if(result==3)
-        { printf("ROLL WAS: %s\n",green);}
+        { printf("|ROLL WAS: %s\n",green);}
 
-        printf("your current money is: $%d\n",money);
-        printf("press enter to continue...");
+        printf("|your current money is: $%d\n",money);
+        printf("|press enter to continue...");
         fflush(stdin);
 
 
@@ -250,26 +256,42 @@ int roller()
     while (0 < answer/37) {
         for (int i = 0; i <= 35; ++i) {
             printf("      >>>> |Rollin| <<<<");
-            printf("\n      |----------------|");
+            printf("\n      |-<---<---<---<--|");
             printf("\n      | %3d < %2d > %2d  |", wheel[i], wheel[i + 1], wheel[i + 2]);
-            printf("\n      |----------------|");
-            Sleep(130);
+            printf("\n      |-<---<---<---<--|");
+            Sleep(65);
+            system("cls");
+
+            printf("      >>>> |Rollin| <<<<");
+            printf("\n      |---<---<---<---<|");
+            printf("\n      | %3d < %2d > %2d  |", wheel[i], wheel[i + 1], wheel[i + 2]);
+            printf("\n      |---<---<---<---<|");
+            Sleep(65);
             system("cls");
         }
 
         answer -= 37;
     }
-
+    int result=0;
     for (int i = 0; i <= answer - 1; ++i) {
         printf("      >>>> |Rollin| <<<<");
-        printf("\n      |----------------|");
+        printf("\n      |-<---<---<---<--|");
         printf("\n      | %3d < %2d > %2d  |", wheel[i], wheel[i + 1], wheel[i + 2]);
-        printf("\n      |----------------|");
-        Sleep(220);
+        printf("\n      |-<---<---<---<--|");
+        Sleep(110);
         system("cls");
+
+        printf("      >>>> |Rollin| <<<<");
+        printf("\n      |---<---<---<---<|");
+        printf("\n      | %3d < %2d > %2d  |", wheel[i], wheel[i + 1], wheel[i + 2]);
+        printf("\n      |---<---<---<---<|");
+        Sleep(110);
+        system("cls");
+        result = wheel[i+1];
     }
 
-    return answer;
+
+    return result;
 }
 
 
